e200ee9fd8f281336d1888bfb59c35fc7cd9222e
XElderX <dalius.badley@gmail.com>
2024-12-11 20:25:37 +0200

    added restart funtionallity

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -28,64 +28,18 @@ class PlayScene extends GameScene {
   create() {
     this.createEnviroment();
     this.createPlayer();
+    this.createObstacles();
+    this.createGameoverContainer();
 
-    this.obstacles = this.physics.add.group();
-
-    this.gameOverText = this.add.image(0, 0, "game-over");
-    this.restartText = this.add.image(0, 80, "restart");
-
-    this.gameOverContainer = this.add
-      .container(this.gameWidth / 2, (this.gameHeight / 2) - 50)
-      .add([this.gameOverText, this.restartText])
-      .setAlpha(0);
-
-    this.startTrigger = this.physics.add.sprite(0, 10, null)
-    .setAlpha(0)
-    .setOrigin(0, 1);
-
-    this.physics.add.collider(this.obstacles, this.player, () => {
-      this.isGameRunning = false;
-      this.physics.pause();
-
-      this.player.die();
-      this.gameOverContainer.setAlpha(1);
-
-      this.spawnTime = 0;
-      this.gameSpeed = 5;
-    });
-
-    this.physics.add.overlap(this.startTrigger, this.player, () => {
-      if (this.startTrigger.y === 10) {
-        this.startTrigger.body.reset(0, this.gameHeight)
-
-        return; 
-      }
-    this.startTrigger.body.reset(9999, 9999);
-      
-      const rollOutEvent = this.time.addEvent({
-        delay: 1000 / 60,
-        loop: true,
-        callback: () => {
-          this.player.playRunAnimation();
-          this.player.setVelocityX(80);
-          this.ground.width += (17 * 2);
-            
-          if (this.ground.width >= this.gameWidth) {
-            rollOutEvent.remove();
-            this.ground.width = this.gameWidth;
-            this.player.setVelocityX(0);
-            this.isGameRunning = true;
-          }  
-        }
-      })
-      // this.shouldStartRoll = true;
-    });
+    this.handleGameStart();
+    this.handeObstacleCollisions();
+    this.handleGameRestart();
   }
 
   update(time: number, delta: number): void {
     if (!this.isGameRunning) { return; }
-    
-  this.spawnTime += delta;
+
+    this.spawnTime += delta;
     if (this.spawnTime >= this.spawnInterval) {
       this.spawnObstacle();
       this.spawnTime = 0;
@@ -114,10 +68,87 @@ class PlayScene extends GameScene {
     const distance = Phaser.Math.Between(600, 900);
 
     this.obstacles
-    .create(distance, this.gameHeight, `obstacle-${obstacleNum}`)
-    .setOrigin(0,1)
-    .setImmovable();
+      .create(distance, this.gameHeight, `obstacle-${obstacleNum}`)
+      .setOrigin(0, 1)
+      .setImmovable();
   }
-}
 
+  createObstacles() {
+    this.obstacles = this.physics.add.group();
+  }
+
+  createGameoverContainer() {
+
+    this.gameOverText = this.add.image(0, 0, "game-over");
+    this.restartText = this.add.image(0, 80, "restart").setInteractive();
+
+    this.gameOverContainer = this.add
+      .container(this.gameWidth / 2, (this.gameHeight / 2) - 50)
+      .add([this.gameOverText, this.restartText])
+      .setAlpha(0);
+
+  }
+
+  handleGameStart() {
+    this.startTrigger = this.physics.add.sprite(0, 10, null)
+      .setAlpha(0)
+      .setOrigin(0, 1);
+
+    this.physics.add.overlap(this.startTrigger, this.player, () => {
+      if (this.startTrigger.y === 10) {
+        this.startTrigger.body.reset(0, this.gameHeight)
+
+        return;
+      }
+      this.startTrigger.body.reset(9999, 9999);
+
+      const rollOutEvent = this.time.addEvent({
+        delay: 1000 / 60,
+        loop: true,
+        callback: () => {
+          this.player.playRunAnimation();
+          this.player.setVelocityX(80);
+          this.ground.width += (17 * 2);
+
+          if (this.ground.width >= this.gameWidth) {
+            rollOutEvent.remove();
+            this.ground.width = this.gameWidth;
+            this.player.setVelocityX(0);
+            this.isGameRunning = true;
+          }
+        }
+      })
+      // this.shouldStartRoll = true;
+    });
+  };
+
+  handleGameRestart() {
+
+    this.restartText.on("pointerdown", () => {
+
+      this.physics.resume();
+      this.player.setVelocityY(0);
+
+      this.obstacles.clear(true, true);
+      this.gameOverContainer.setAlpha(0);
+      this.anims.resumeAll();
+      this.isGameRunning = true;
+    });
+  };
+
+  handeObstacleCollisions() {
+
+    this.physics.add.collider(this.obstacles, this.player, () => {
+      this.isGameRunning = false;
+      this.physics.pause();
+
+      this.player.die();
+      this.gameOverContainer.setAlpha(1);
+
+      this.spawnTime = 0;
+      this.gameSpeed = 5;
+    });
+  };
+
+}
 export default PlayScene;
